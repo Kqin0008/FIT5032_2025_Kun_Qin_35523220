80594e496a874e3e47933331328b27ec5bd5edb3
kqin <kqin0008@student.monash.edu>
2025-07-08 18:58:37 +0800

    week3

--- kqin-library/src/App.vue
+++ kqin-library/src/App.vue
@@ -1,20 +1,13 @@
 <script setup>
 
 import JSON from './components/JSON.vue';
+import Form from "./components/Form.vue";
 
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <JSON/>
+    <Form />
   </main>
 </template>
 
