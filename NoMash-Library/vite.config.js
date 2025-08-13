7a98ee76698244fab5c05ec31f8944cbab063df1
kqin <kqin0008@student.monash.edu>
2025-08-13 15:50:51 +0800

    vite.config.js update

--- NoMash-Library/vite.config.js
+++ NoMash-Library/vite.config.js
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,11 +14,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
+  },
+  base: process.env.NODE_ENV === 'production'
     ? '/FIT5032-week5/'
     : '/'
-}
+});
