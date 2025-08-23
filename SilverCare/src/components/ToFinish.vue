0b350185a09fa58c5a222aeaa9c041a57da29dec
kqin <kqin0008@student.monash.edu>
2025-08-23 10:27:30 +0800

    submit first

---
+++ SilverCare/src/components/ToFinish.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="to-finish">
+    <h2>To Finish</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToFinish',
+}
+</script>
+
+<style scoped>
+.to-finish {
+  margin-top: 2rem;
+}
+</style>
+
